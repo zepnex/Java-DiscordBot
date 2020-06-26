49291e3f9be5e4c19255fe6650b96de52760abbc
zepnex <zepnex@users.noreply.github.com>
2020-06-26 23:35:27 +0200

    Changed DiscordBot.java
    Now read your Bot-Token from a .json file

---
+++ DiscordBot/src/main/java/dev/zepnex/BotInformation.java
@@ -0,0 +1,12 @@
+package dev.zepnex;
+
+public class BotInformation {
+    private String token;
+
+    public BotInformation(String token) {
+        this.token = token;
+    }
+    public String getToken(){
+        return token;
+    }
+}
--- DiscordBot/src/main/java/dev/zepnex/DiscordBot.java
+++ DiscordBot/src/main/java/dev/zepnex/DiscordBot.java
@@ -1,5 +1,6 @@
 package dev.zepnex;
 
+import com.google.gson.Gson;
 import dev.zepnex.listener.CommandListener;
 import dev.zepnex.listener.CommandManager;
 import net.dv8tion.jda.api.JDABuilder;
@@ -7,22 +8,32 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 
 public class DiscordBot {
     public static DiscordBot INSTANCE;
+    private static BotInformation inf;
     public JDABuilder builder;
     private CommandManager cmdMan;
+    static String path = "<Your path to the directory of your .json file>";
 
     public static void main(String[] args) throws LoginException {
+        Gson gson = new Gson();
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new FileReader(path + "BotInf.json"));
+            inf = gson.fromJson(reader, BotInformation.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         new DiscordBot();
     }
 
     public DiscordBot() throws LoginException {
+
         INSTANCE = this;
-        builder = new JDABuilder("NzI0MzU4OTM5NDU3ODgwMDc2.XvE1iw.wQIGkd_gvZ3WoZcf4iB2Gp02WWU");
+        builder = new JDABuilder(inf.getToken());
         this.cmdMan = new CommandManager();
         builder.addEventListeners(new CommandListener());
         builder.setActivity(Activity.playing("Mooooin Maister"));
